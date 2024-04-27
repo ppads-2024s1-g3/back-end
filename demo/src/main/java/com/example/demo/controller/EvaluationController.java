ee04785ecbaf8b85d616d39c91b219b255b865ee
Gustavo R. Carvalho <147208728+grcarvalho1032@users.noreply.github.com>
2024-04-26 23:22:03 -0300

    evaluation

--- demo/src/main/java/com/example/demo/controller/EvaluationController.java
+++ demo/src/main/java/com/example/demo/controller/EvaluationController.java
@@ -4,14 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,20 +48,4 @@ public class EvaluationController {
         }
         return false;
     }
-
- 
-
-    @PutMapping("/evaluations/{id}")
-    public ResponseEntity<Evaluation> updateEvaluation(@PathVariable(value = "id") long id, @RequestBody Evaluation newEvaluation) {
-        Optional<Evaluation> oldEvaluation = repository.findById(id);
-        if (oldEvaluation.isPresent()) {
-            Evaluation evaluation = oldEvaluation.get();
-            evaluation.setContent(newEvaluation.getContent());
-            evaluation.setUser(newEvaluation.getUser());
-            repository.save(evaluation);
-            return new ResponseEntity<Evaluation>(evaluation, HttpStatus.OK);
-        } else
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
 }
